c64daaab1c7b99bd832e9d458fbc5782957422be
Piyush Garg <piyushgarg.dev@gmail.com>
2024-02-10 19:13:55 +0530

    Added Try Catch Error handling

--- controllers/movieSchedule.js
+++ controllers/movieSchedule.js
@@ -5,25 +5,31 @@ const handleGetAllMovieSchedules = async (req, res) => {
     const page = req.query.page ? parseInt(req.query.page) : 1;
     const LIMIT = 5;
 
-    const skip = (page -1) * LIMIT;
+    const skip = (page - 1) * LIMIT;
     const movieSchedules = await MovieSchedule.find({}).skip(skip).limit(LIMIT);
-    
-    return res.json({ status: 'sucsess', data: { page, movieSchedules }});
+
+    return res.json({ status: 'sucsess', data: { page, movieSchedules } });
 };
 
 const handleCreateMovieSchedule = async (req, res) => {
 
     const safeParseResult = validateCreateMovieSchedulePayload(req.body);
 
-    if(safeParseResult.error) {
+    if (safeParseResult.error) {
         return res.status(400).json({ status: 'error', error: safeParseResult.error });
     }
 
     const { movieId, theatreId, startTime, price } = safeParseResult.data;
 
-    const movieSchedule = await MovieSchedule.create({ movieId, theatreId, startTime, price });
+    try {
+        const movieSchedule = await MovieSchedule.create({ movieId, theatreId, startTime, price });
 
-    return res.json({ status: 'success', data: {id: movieSchedule._id} });
+        return res.json({ status: 'success', data: { id: movieSchedule._id } });
+    } catch (err) {
+        if (err.code === 11000)
+            return res.status(400).json({ message: `Schedule already exists!` })
+        return res.status(500).json({ message: 'Internal Server Error' })
+    }
 };
 
 module.exports = { handleCreateMovieSchedule, handleGetAllMovieSchedules }
