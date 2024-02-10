548f78d7c351735dc67045b85d4f4089057ec0c1
mayyu9 <coolmahimsd@gmail.com>
2024-02-10 13:47:51 +0530

    implemented movie schedule routes.

---
+++ routes/movieSchedule.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { ensureAuthenticated } = require('../middlewares/authentication');
+const controller = require('../controllers/movieSchedule');
+
+const router = express.Router();
+
+router.use(ensureAuthenticated(['admin']));
+
+router.get('/', controller.handleGetAllMovieSchedules);
+
+router.get('/:id');
+
+router.post('/', controller.handleCreateMovieSchedule);
+
+router.patch('/:id');
+
+router.delete('/:id');
+
+module.exports = router
